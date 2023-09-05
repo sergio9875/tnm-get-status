7a6e836f1de65ab1386276d2cb1a2aa46bfeac40
SergeyK <sergey.kabachynov@dpogroup.com>
2023-09-05 14:45:12 +0300

    test

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/google/uuid"
-	"io"
+	"io/ioutil"
 	log "malawi-getstatus/logger"
 	"malawi-getstatus/process"
 	"malawi-getstatus/utils"
@@ -34,35 +35,97 @@ func LambdaHandler(ctx context.Context, sqsEvent events.SQSEvent) error {
 
 	log.Info("START PROCESS")
 
-	// JSON body
-	body1 := []byte(`{
-    "wallet": "500957",
-    "password" : "Test_Test_42"
-	}`)
+	//url := "https://jsonplaceholder.typicode.com/users/1"
+	//url := "https://dev.payouts.tnmmpamba.co.mw/api/payments/NT543Y5YT5R432"
 
-	url := "https://dev.payouts.tnmmpamba.co.mw/api/authenticate"
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body1))
-	req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
-
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	//If the struct variable names does not match with json attributes
+	//then you can define the json attributes actual name after json:attname as shown below.
+	type User struct {
+		Name string `json:"name"`
+		Job  string `json:"job"`
+	}
+	type Auth struct {
+		Wallet   string `json:"wallet"`
+		Password string `json:"password"`
 	}
 
-	client := &http.Client{Transport: tr}
-	resp, err := client.Do(req)
+	//Create user struct which need to post.
+	//user := User{
+	//	Name: "Test User1",
+	//	Job:  "Go lang Developer",
+	//}
+	//Create user struct which need to post.
+	pass := Auth{
+		Wallet:   "500957",
+		Password: "Test_Test_42",
+	}
+
+	//Convert User to byte using Json.Marshal
+	//Ignoring error.
+	body, _ := json.Marshal(pass)
+	//body_auth, _ := json.Marshal(pass)
+
+	//Pass new buffer for request with URL to post.
+	//This will make a post request and will share the JSON data
+	//resp, err := http.Post("https://reqres.in/api/users", "application/json", bytes.NewBuffer(body))
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	resp, err := http.Post("https://dev.payouts.tnmmpamba.co.mw/api/authenticate", "application/json", bytes.NewBuffer(body))
+
+	// An error is returned if something goes wrong
 	if err != nil {
 		panic(err)
 	}
+	//Need to close the response stream, once response is read.
+	//Hence defer close. It will automatically take care of it.
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := io.ReadAll(resp.Body)
-	log.Println("response Body:", body)
-	fmt.Println("response Body:", string(body))
+	//Check response code, if New user is created then read response.
+	if resp.StatusCode == http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			//Failed to read response.
+			panic(err)
+		}
+
+		//Convert bytes to String and print
+		jsonStr := string(body)
+
+		fmt.Println("Response: ", jsonStr)
+
+	} else {
+		//The status is not Created. print the error.
+		fmt.Println("Get failed with error: ", resp.Status)
+	}
+
+	//body1 := []byte(`{
+	//"wallet": "500957",
+	//"password" : "Test_Test_42"
+	//}`)
+	//
+	//url := "https://dev.payouts.tnmmpamba.co.mw/api/authenticate"
+	//req, err := http.NewRequest("POST", url, bytes.NewBuffer(body1))
+	//req.Header.Set("X-Custom-Header", "myvalue")
+	//req.Header.Set("Content-Type", "application/json")
+	//
+	//tr := &http.Transport{
+	//	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	//}
+	//
+	//client := &http.Client{Transport: tr}
+	//resp, err := client.Do(req)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer resp.Body.Close()
+	//
+	//fmt.Println("response Status:", resp.Status)
+	//fmt.Println("response Headers:", resp.Header)
+	//body, _ := io.ReadAll(resp.Body)
+	//log.Println("response Body:", body)
+	//fmt.Println("response Body:", string(body))
 
 	log.Info("END PROCESS")
+	os.Exit(2)
 
 	log.Debug("ROOT", "version: <GIT_HASH>")
 
