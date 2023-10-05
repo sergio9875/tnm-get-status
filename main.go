5bf34ed18ded16731a0399ada8f758b301df1314
SergeyK <sergey.kabachynov@dpogroup.com>
2023-10-05 12:36:48 +0300

    test

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"crypto/tls"
-	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/google/uuid"
-	"io/ioutil"
+	"io"
 	log "malawi-getstatus/logger"
 	"malawi-getstatus/process"
 	"malawi-getstatus/utils"
 	"net/http"
 	"os"
+	"reflect"
 )
 
 var invokeCount = 0
@@ -35,94 +34,99 @@ func LambdaHandler(ctx context.Context, sqsEvent events.SQSEvent) error {
 
 	log.Info("START PROCESS")
 
-	//url := "https://jsonplaceholder.typicode.com/users/1"
-	//url := "https://dev.payouts.tnmmpamba.co.mw/api/payments/NT543Y5YT5R432"
-
-	//If the struct variable names does not match with json attributes
-	//then you can define the json attributes actual name after json:attname as shown below.
-	type User struct {
-		Name string `json:"name"`
-		Job  string `json:"job"`
-	}
 	type Auth struct {
 		Wallet   string `json:"wallet"`
 		Password string `json:"password"`
 	}
 
-	//Create user struct which need to post.
-	//user := User{
-	//	Name: "Test User1",
-	//	Job:  "Go lang Developer",
-	//}
-	//Create user struct which need to post.
 	pass := Auth{
 		Wallet:   "500957",
 		Password: "Test_Test_42",
 	}
+	type TokenRes struct {
+		Token     string `json:"token,omitempty"`
+		ExpiresAt string `json:"expires_at,omitempty"`
+	}
+	type TokenResponse struct {
+		Message string      `json:"message,omitempty"`
+		Errors  interface{} `json:"errors,omitempty"`
+		Trace   interface{} `json:"trace,omitempty"`
+		Data    TokenRes    `json:"data,omitempty"`
+	}
 
 	//Convert User to byte using Json.Marshal
 	//Ignoring error.
-	body, _ := json.Marshal(pass)
+	fmt.Println(reflect.TypeOf(pass))
+
+	//	body, _ := json.Marshal(pass)
 	//body_auth, _ := json.Marshal(pass)
 
 	//Pass new buffer for request with URL to post.
 	//This will make a post request and will share the JSON data
 	//resp, err := http.Post("https://reqres.in/api/users", "application/json", bytes.NewBuffer(body))
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	resp, err := http.Post("https://dev.payouts.tnmmpamba.co.mw/api/authenticate", "application/json", bytes.NewBuffer(body))
-
-	// An error is returned if something goes wrong
+	client := &http.Client{}
+	bearer := "970|laravel_sanctum_oKZCbpWyIGwQ5eBuGYmU2kWePZnKfkp3OOuyXJFW49f85549"
+	req, err := http.NewRequest("GET", "https://dev.payouts.tnmmpamba.co.mw/api/invoices/1000955", nil)
 	if err != nil {
-		panic(err)
-	}
-	//Need to close the response stream, once response is read.
-	//Hence defer close. It will automatically take care of it.
-	defer resp.Body.Close()
-
-	//Check response code, if New user is created then read response.
-	if resp.StatusCode == http.StatusOK {
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			//Failed to read response.
-			panic(err)
-		}
-
-		//Convert bytes to String and print
-		jsonStr := string(body)
-
-		fmt.Println("Response: ", jsonStr)
-
-	} else {
-		//The status is not Created. print the error.
-		fmt.Println("Get failed with error: ", resp.Status)
+		// handle error
 	}
 
-	//body1 := []byte(`{
-	//"wallet": "500957",
-	//"password" : "Test_Test_42"
-	//}`)
-	//
-	//url := "https://dev.payouts.tnmmpamba.co.mw/api/authenticate"
-	//req, err := http.NewRequest("POST", url, bytes.NewBuffer(body1))
-	//req.Header.Set("X-Custom-Header", "myvalue")
-	//req.Header.Set("Content-Type", "application/json")
-	//
-	//tr := &http.Transport{
-	//	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	//}
-	//
-	//client := &http.Client{Transport: tr}
-	//resp, err := client.Do(req)
+	req.Header.Set("Content-Type", "application/json")
+
+	// add authorization header to the req
+	req.Header.Add("Authorization", bearer)
+	res, err := client.Do(req)
+	if err != nil {
+		// handle error
+	}
+
+	responseBody, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	jsonStr := string(responseBody)
+	fmt.Println("Status: ", res.Status)
+	fmt.Println("Response body: ", jsonStr)
+	//resp, err := http.Post("https://dev.payouts.tnmmpamba.co.mw/api/authenticate", "application/json", bytes.NewBuffer(body))
+	////req.Header.Add("Authorization", "Bearer ...")
+	//// An error is returned if something goes wrong
 	//if err != nil {
 	//	panic(err)
 	//}
+	////Need to close the response stream, once response is read.
+	////Hence defer close. It will automatically take care of it.
 	//defer resp.Body.Close()
 	//
-	//fmt.Println("response Status:", resp.Status)
-	//fmt.Println("response Headers:", resp.Header)
-	//body, _ := io.ReadAll(resp.Body)
-	//log.Println("response Body:", body)
-	//fmt.Println("response Body:", string(body))
+	////Check response code, if New user is created then read response.
+	//if resp.StatusCode == http.StatusOK {
+	//	body, err := ioutil.ReadAll(resp.Body)
+	//	if err != nil {
+	//		//Failed to read response.
+	//		panic(err)
+	//	}
+	//
+	//	//Convert bytes to String and print
+	//	jsonStr := string(body)
+	//
+	//	fmt.Println("Response: ", jsonStr)
+	//
+	//	//client := &http.Client{}
+	//
+	//	//var tokenResponse TokenResponse
+	//	//
+	//	//err = json.Unmarshal(body, &tokenResponse)
+	//	//if err != nil {
+	//	//	log.Error("Failed to unmarshal Response_Token: ", err.Error())
+	//	//	return nil
+	//	//}
+	//
+	//} else {
+	//	//The status is not Created. print the error.
+	//	fmt.Println("Get failed with error: ", resp.Status)
+	//}
 
 	log.Info("END PROCESS")
 	os.Exit(2)
